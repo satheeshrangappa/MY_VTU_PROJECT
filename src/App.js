c325805a79cf78bca11de2dac7e2053d966b0e95
satheeshrangappa <rsathishec036@gmail.com>
2025-01-06 00:31:10 +0530

    Footer Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
-
+import { Container } from "react-bootstrap";
 function App() {
   return (
     <>
       <Header />
+      <Container>
+        <main>
+          
+        </main>
+      </Container>
+      <Footer />
     </>
   );
 }
